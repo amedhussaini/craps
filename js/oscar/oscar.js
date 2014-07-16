b853e00bccd8a3aceaacae2a97a2ded397553373
Amed Hussaini <amedhussaini@gmail.com>
2014-07-15 23:00:53 -0400

    Removed interval.  Debugging through console.

--- js/oscar/oscar.js
+++ js/oscar/oscar.js
@@ -9,40 +9,85 @@ function loadUser() {
 window.OscarsGrind = (function() {
 
 	//private variables
-
-	var _bankroll = null;
-	var _betUnit = null;
-	var _betSize = null
+	randomRoll = function() {
+		return Math.floor(Math.random() * 6) + 1;
+	}
 
 	return {
 
 		d1: null,
 		d2: null,
-
+		bankroll: null,
+		betUnit: null,
+		betSize: null,
+		point: null,
+		comeOut: true,
+		comeOutRoll: null,
 			//public methods
 		rollDice: function() {
-			this.d1 = Math.floor(Math.random() * 6) + 1;
-			this.d2 = Math.floor(Math.random() * 6) + 1;
+			this.d1 = randomRoll();
+			this.d2 = randomRoll();
+
+
 		},
-		setBankroll: function(bankroll) {
+		setBankroll: function(_bankroll) {
 
-			this._bankroll = bankroll;
+			this.bankroll = _bankroll;
 
+		},
+		setBetSize: function(_betSize) {
+			this.betSize = _betSize;
+		},
+		setPoint: function(_point) {
+			this.point = _point;
+		},
+		comeOutRoll: function() {
+			var d1 = randomRoll();
+			var d2 = randomRoll();
+			var roll = d1 + d2;
+			if (roll == 7 || roll == 11 && this.comeOut == true) {
+				this.comeOutRoll = roll;
+				OscarsGrind.natural();
+			} else {
+				this.point = roll;
+				this.comeOut = false;
+			}
+		},
+		natural: function() {
+			this.bankroll += this.betUnit * this.betSize;
+			this.comeOut == true;
 		},
 		getBankroll: function() {
 			console.log(this._bankroll);
 		},
-		setBetUnit: function(betunit) {
-			this._betUnit = betunit;
+		setBetUnit: function(_betunit) {
+			this.betUnit = _betunit;
 		},
 		getBetUnit: function() {
-			console.log(this._betUnit);
+			console.log(this.betUnit);
 		},
-		loadUser: function() {
+		setDiceView: function() {
 		 	var template = $('#template').html();
 		 	Mustache.parse(template);   // optional, speeds up future uses
-		 	var rendered = Mustache.render(template, {name: this.d1, name2: this.d2});
+		 	var rendered = Mustache.render(template, {dice1: this.d1, dice2: this.d2});
 		 	$('#target').html(rendered);
+		},
+		start: function() {
+
+			OscarsGrind.setBankroll(1000);
+			OscarsGrind.setBetUnit(1);
+			OscarsGrind.setBetSize(10);
+			OscarsGrind.comeOutRoll();
+
+			/*
+			setInterval(function(){
+
+				OscarsGrind.rollDice();
+				OscarsGrind.setDiceView();
+
+
+			}, 2000);
+*/
 		}
 
 
@@ -51,5 +96,6 @@ window.OscarsGrind = (function() {
 
 })();
 
-OscarsGrind.rollDice();
+
+OscarsGrind.start();
 
